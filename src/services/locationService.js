223a4fe7d3dfa195576e046adabb656e90d5ea6c
galaridor <milenpenkov1@gmail.com>
2024-10-10 00:05:48 +0300

    fix location defect

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,14 +1,12 @@
 import * as request from "../utils/request";
 
 const baseUrl = "https://api.opencagedata.com/geocode/v1/json";
-const apiKey = '00eb2aee58b742b78b6d94143d8b61d9';
+const apiKey = 'e88cbacd684142209a5d8d88e82544f1';
 
 export const getLocationByAddress = async (address) => {
 
     try {
         const result = await request.get(`${baseUrl}?q=${address}&key=${apiKey}`)
-        
-        console.log(result);
 
         return result;
     } 
